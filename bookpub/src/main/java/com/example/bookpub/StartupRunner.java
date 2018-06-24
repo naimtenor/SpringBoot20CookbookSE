0ce0509ab750f896d3e9387b76f5a47375342020
naimtenor <naimtenor@SketchBook>
2018-06-25 01:07:03 +0900

    자동으로 생성되는 메모리 기반의 H2 데이터베이스 인스턴스 생성

--- bookpub/src/main/java/com/example/bookpub/StartupRunner.java
+++ bookpub/src/main/java/com/example/bookpub/StartupRunner.java
@@ -1,16 +1,22 @@
 package com.example.bookpub;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
 public class StartupRunner implements CommandLineRunner {
 	
 	protected final Log logger = LogFactory.getLog(getClass());
+	
+	@Autowired
+	private DataSource ds;
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("Hello");
+		logger.info("DataSource: " + ds.toString());
 	}
 
 }
