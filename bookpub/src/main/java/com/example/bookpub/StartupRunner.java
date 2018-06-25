76c4d3bf9eaeb57badc3564dd961601f7a2ff6bf
naimtenor <naimtenor@SketchBook>
2018-06-26 02:14:59 +0900

    하이버네이트를 사용한 orm 실습

--- bookpub/src/main/java/com/example/bookpub/StartupRunner.java
+++ bookpub/src/main/java/com/example/bookpub/StartupRunner.java
@@ -1,22 +1,30 @@
 package com.example.bookpub;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import com.example.bookpub.repository.BookRepository;
+
 public class StartupRunner implements CommandLineRunner {
 	
 	protected final Log logger = LogFactory.getLog(getClass());
 	
+	/*// 데이터 소스 정보를 출력
 	@Autowired
 	private DataSource ds;
 
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("DataSource: " + ds.toString());
-	}
+	}*/
 
+	// 책의 개수를 출력
+	@Autowired
+	private BookRepository bookRepository;
+
+	public void run(String... args) throws Exception {
+		logger.info("책의 개수 : " + bookRepository.count());
+	}
 }
