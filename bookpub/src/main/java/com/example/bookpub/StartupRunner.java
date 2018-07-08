22292a12d5b57fc8697e20dd17ca51dfb26a39aa
naimtenor <naimtenor@gmail.com>
2018-07-08 18:08:51 +0900

    chapter05-base

--- bookpub/src/main/java/com/example/bookpub/StartupRunner.java
+++ bookpub/src/main/java/com/example/bookpub/StartupRunner.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import com.example.bookpub.entity.Author;
+import com.example.bookpub.entity.Book;
+import com.example.bookpub.entity.Publisher;
+import com.example.bookpub.repository.AuthorRepository;
 import com.example.bookpub.repository.BookRepository;
+import com.example.bookpub.repository.PublisherRepository;
 
 public class StartupRunner implements CommandLineRunner {
 	
@@ -24,9 +29,20 @@ public class StartupRunner implements CommandLineRunner {
 	// 책의 개수를 출력
 	@Autowired
 	private BookRepository bookRepository;
+	@Autowired 
+	private AuthorRepository authorRepository;
+    @Autowired 
+    private PublisherRepository publisherRepository;
 
 	public void run(String... args) throws Exception {
-		logger.info("책의 개수 : " + bookRepository.count());
+		//logger.info("책의 개수 : " + bookRepository.count());
+		logger.info("도서 관리 시스템에 오신 것을 환영합니다.");
+        Author author = new Author("Alex", "Antonov");
+        author = authorRepository.save(author);
+        Publisher publisher = new Publisher("Packt");
+        publisher = publisherRepository.save(publisher);
+        Book book = new Book("978-1-78528-415-1", "Spring Boot Recipes", author, publisher);
+        bookRepository.save(book);
 	}
 	
 	// 예약 작업 실행기를 사용한 주기적 책의 개수 출력
