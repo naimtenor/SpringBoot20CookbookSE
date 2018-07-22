e9a327f14f31aa25854d47ada6caf91e132717fc
naimtenor <naimtenor@gmail.com>
2018-07-23 01:42:15 +0900

    목 객체를 사용한 테스트케이스 생성

---
+++ bookpub/src/test/java/com/example/bookpub/MockPublisherRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.example.bookpub;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.reset;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.bookpub.repository.PublisherRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class MockPublisherRepositoryTests {
+	@MockBean
+	private PublisherRepository repository;
+
+	@Before
+	public void setupPublisherRepositoryMock() {
+		given(repository.count()).willReturn(5L);
+	}
+
+	@Test
+	public void publishersExist() {
+		assertThat(repository.count()).isEqualTo(5L);
+	}
+
+	@After
+	public void resetPublisherRepositoryMock() {
+		reset(repository);
+	}
+}
